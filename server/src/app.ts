e00335725b6eed08d665c815f144ea454cd02e1b
Ricardoamorim0 <Ricardoamorim0.2001@gmail.com>
2023-08-08 19:06:38 +0100

    feat: added database support

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,9 @@ import express, { Express } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import morgan from 'morgan';
+import postgres from 'pg';
+import constants from './constants/constants.js';
+import 'dotenv/config';
 
 import routes from './routes/routes.js';
 
@@ -9,8 +12,14 @@ const app: Express = express();
 
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-routes(app);
+const pool = new postgres.Pool({ connectionString: constants.DB_CONNECTION_STRING });
+pool.connect().then(client => {
+  routes(app, client);
+}).catch(error => {
+  console.log(error);
+});
 
-app.listen(process.env.PORT ||  8081);
+app.listen(constants.PORT);
--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,16 +1,79 @@
 import { Express, Request, Response } from 'express';
-export default (app: Express) => {
+import postgres from 'pg';
+import constants from '../constants/constants.js';
+import 'dotenv/config';
 
+import db from '../database/database.js';
+
+export default (app: Express, client: postgres.PoolClient) => {
+
+  // Set database client
+  db.set(client);
+
+
+  // Routes
   app.post('/register', (req: Request, res: Response) => {
-    res.json({
-      message: 'User have been registered!'
-    });
+
+    if (typeof req.body.name === 'string' &&
+    typeof req.body.email === 'string' &&
+    typeof req.body.password === 'string') {
+
+      const name = req.body.name.trim();
+      const email = req.body.email.trim();
+      const password = req.body.password.trim();
+      
+      if (name.length >= constants.MIN_NAME_LENGTH && email.length >= constants.MIN_EMAIL_LENGTH && password.length >= constants.MIN_PASSWORD_LENGTH) {
+        if (name.length <= constants.MAX_NAME_LENGTH && email.length <= constants.MAX_EMAIL_LENGTH && password.length <= constants.MAX_PASSWORD_LENGTH) {
+          return db.checkIfUserExists(email).then((userExists) => {
+            if (userExists) {
+              res.json(output(false, 'db_cu_0'));
+            } else {
+              db.createUser(name, email, password);
+              res.json(output(true, 'done'));
+            }
+          }).catch((error) => {
+            res.json(output(false, 'unknown'));
+            console.log(error);
+          });
+        }
+      }
+    }
+
+    res.json(output(false, 'db_0'));
+    
   });
 
   app.post('/login', (req: Request, res: Response) => {
-    res.json({
-      message: 'User tried to login!'
-    });
+
+    if (typeof req.body.email === 'string' &&
+    typeof req.body.password === 'string') {
+
+      const email = req.body.email.trim();
+      const password = req.body.password.trim();
+
+      if (email.length >= constants.MIN_EMAIL_LENGTH && password.length > 0) {
+        return db.checkIfUserExists(email).then((userExists: boolean) => {
+          if (!userExists) {
+            res.json(output(false, 'db_lu_0'));
+            return;
+          }
+
+          return db.isUserPassword(email, password).then((isUserPassword: boolean) => {
+            res.json(isUserPassword? output(true, 'done') : output(false, 'db_lu_0'));
+            return;
+          }).catch((error) => { throw error; });
+        }).catch((error) => {
+          res.json(output(false, 'unknown'));
+          console.log(error);
+        });
+      }
+    }
+
+    res.json(output(false, 'db_0'));
   });
 
 };
+
+function output(success: boolean, errorCode: string): { success: boolean, error: string, message: string } {
+  return {success: success, error: errorCode, message: constants.ERRORS[errorCode] || ''};
+}
